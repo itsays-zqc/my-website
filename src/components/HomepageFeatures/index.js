a8c68eada1c5ba72c58a2b4966cb527624405e29
zhan-chen6 <3115837926@qq.com>
2023-08-14 18:56:49 +0800

    1

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -28,7 +28,7 @@ const FeatureList = [
 
         <hr style={{ margin: '0.25rem 0' }}/>
         <div style={{ textAlign: 'left' }}>
-          <a href="/my-website/docs/faq/Installation">    3. Physics </a>
+          <a href="/my-website/docs/faq/Physics">    3. Physics </a>
         </div>
 
 
